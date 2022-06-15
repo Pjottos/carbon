a75f665e712a4f5236b1b9dee261672f6e11283a
Pjottos <pjottos@proton.me>
2022-06-15 22:50:38 +0200

    Create gateway for clients to bind to

---
+++ src/gateway.rs
@@ -0,0 +1,77 @@
+use nix::fcntl::{flock, FlockArg};
+use tokio::net::UnixListener;
+
+use std::{env, fs, io, os::unix::prelude::*, path::PathBuf};
+
+pub struct Gateway {
+    _lock_file: fs::File,
+    listener: UnixListener,
+}
+
+impl Gateway {
+    pub fn new() -> Self {
+        let mut path: PathBuf = env::var("XDG_RUNTIME_DIR")
+            .ok()
+            .filter(|dir| dir.starts_with('/'))
+            .expect("XDG_RUNTIME_DIR not set or invalid")
+            .into();
+
+        for n in 0..32 {
+            if n != 0 {
+                path.pop();
+            }
+            path.push(format!("wayland-{}.lock", n));
+
+            let lock_file = match fs::OpenOptions::new()
+                .create(true)
+                .read(true)
+                .write(true)
+                .mode(0o660)
+                .open(&path)
+            {
+                Ok(f) => f,
+                Err(e) => {
+                    log::warn!(
+                        "Failed to open socket lock file at {}: {}",
+                        path.to_string_lossy(),
+                        e
+                    );
+                    continue;
+                }
+            };
+
+            if let Err(e) = flock(lock_file.as_raw_fd(), FlockArg::LockExclusiveNonblock) {
+                log::warn!(
+                    "Failed to acquire socket lock at {}: {}",
+                    path.to_string_lossy(),
+                    e.desc()
+                );
+                continue;
+            };
+
+            path.set_extension("");
+            if let Some(e) = fs::remove_file(&path)
+                .err()
+                .filter(|e| e.kind() != io::ErrorKind::NotFound)
+            {
+                log::warn!(
+                    "Failed to remove existing socket at {}: {}",
+                    path.to_string_lossy(),
+                    e
+                );
+                continue;
+            }
+
+            let listener = UnixListener::bind(&path).expect("Failed to bind to socket");
+
+            log::info!("Bound socket at {}", path.to_string_lossy());
+
+            return Self {
+                _lock_file: lock_file,
+                listener,
+            };
+        }
+
+        panic!("Could not find a socket to bind to");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-fn main() {
+mod gateway;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     env_logger::init();
 
     log::info!("Starting carbon...");
+
+    let gateway = gateway::Gateway::new();
 }
