3aa18f1376b1636c4e6f56a911dadd1ca2ab417f
Pjottos <pjottos@proton.me>
2022-06-21 10:43:41 +0200

    Allow running protocol scanner to easily debug generated code

--- protocol-scanner/src/main.rs
+++ protocol-scanner/src/main.rs
@@ -1,4 +1,4 @@
-use protocol_scanner::{emit_stubs, ProtocolParser};
+use protocol_scanner::{emit_stubs, CodeBuilder, ProtocolParser};
 
 use std::{env, fs};
 
@@ -11,17 +11,24 @@ fn main() {
     let target_path = args
         .next()
         .expect("second arg should be target path or '-' for stdout");
+    let gen_stubs = args.next().map_or(true, |arg| arg != "debug_codegen");
 
     let mut parser = ProtocolParser::new(protocol_path.as_ref());
     while parser.next(&mut buf) {}
     let protocol = parser.finish();
 
-    let (_name, tokens) = emit_stubs(protocol);
+    let tokens = if gen_stubs {
+        emit_stubs(&protocol)
+    } else {
+        let mut builder = CodeBuilder::default();
+        builder.add_protocol(protocol);
+        builder.build()
+    };
 
     if target_path == "-" {
         println!("{}", tokens);
     } else {
         let code = format!("{}", tokens);
-        fs::write(target_path, code.as_bytes()).expect("failed to write stubs");
+        fs::write(target_path, code.as_bytes()).expect("failed to write output");
     }
 }
