a16d55051e17b6ec80e66c0deb850f78e7980f0d
Pjottos <pjottos@proton.me>
2022-06-21 17:45:48 +0200

    Allow generating code for entire directory of protocols

--- protocol-scanner/src/main.rs
+++ protocol-scanner/src/main.rs
@@ -1,10 +1,8 @@
-use protocol_scanner::{emit_stubs, CodeBuilder, ProtocolParser};
+use protocol_scanner::{emit_stubs, CodeBuilder, Protocol, ProtocolParser};
 
-use std::{env, fs};
+use std::{env, fs, path::Path};
 
 fn main() {
-    let mut buf = vec![];
-
     let mut args = env::args();
     args.next();
     let protocol_path = args.next().expect("first arg should be protocol path");
@@ -13,17 +11,26 @@ fn main() {
         .expect("second arg should be target path or '-' for stdout");
     let gen_stubs = args.next().map_or(true, |arg| arg != "debug_codegen");
 
-    let mut parser = ProtocolParser::new(protocol_path.as_ref());
-    while parser.next(&mut buf) {}
-    let protocol = parser.finish();
-
-    let tokens = if gen_stubs {
-        emit_stubs(&protocol)
+    let tokens;
+    if gen_stubs {
+        let protocol = parse_protocol(&protocol_path);
+        tokens = emit_stubs(&protocol);
     } else {
-        let mut builder = CodeBuilder::default();
-        builder.add_protocol(protocol);
-        builder.build()
-    };
+        if fs::metadata(&protocol_path).unwrap().is_dir() {
+            let mut builder = CodeBuilder::default();
+            for entry in fs::read_dir(&protocol_path).unwrap() {
+                let entry = entry.unwrap();
+                let protocol = parse_protocol(entry.path());
+                builder.add_protocol(protocol);
+            }
+            tokens = builder.build();
+        } else {
+            let mut builder = CodeBuilder::default();
+            let protocol = parse_protocol(&protocol_path);
+            builder.add_protocol(protocol);
+            tokens = builder.build();
+        }
+    }
 
     if target_path == "-" {
         println!("{}", tokens);
@@ -31,4 +38,12 @@ fn main() {
         let code = format!("{}", tokens);
         fs::write(target_path, code.as_bytes()).expect("failed to write output");
     }
+}
+
+fn parse_protocol<P: AsRef<Path>>(path: P) -> Protocol {
+    let mut buf = vec![];
+
+    let mut parser = ProtocolParser::new(path.as_ref());
+    while parser.next(&mut buf) {}
+    parser.finish()
 }
